1a4a7dee5f633f52da18d1093346d9b46aab2fc8
rkyzk <reikoyazaki@yahoo.com>
2023-07-08 04:55:52 +0000

    write profiles.views and add urls in api.urls.py and profiles.urls.py

--- profiles/views.py
+++ profiles/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.db.models import Count
+from rest_framework import generics, filters
+from django_filters.rest_framework import DjangoFilterBackend
+from .models import Profile
+from .serializers import ProfileSerializer
 
-# Create your views here.
+
+class ProfileList(generics.ListAPIView):
+    """
+    List all profiles.
+    No create view as profile creation is handled by django signals.
+    """
+    queryset = Profile.objects.all().order_by('-created_at')
+    serializer_class = ProfileSerializer
+
