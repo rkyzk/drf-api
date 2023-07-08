3e2f809ab46a0af93e81c8d1e23b749cf6097a1a
rkyzk <reikoyazaki@yahoo.com>
2023-07-08 06:18:38 +0000

    add like_id, likes_count & comments_count in poems.serializers and add filter, search and ordering fields in poems.views

--- poems/serializers.py
+++ poems/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from poems.models import Poem
+from likes.models import Like
 from datetime import datetime
 
 
@@ -8,20 +9,32 @@ class PoemSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
-    profile_name = serializers.ReadOnlyField(
-                       source='owner.profile.display_name')
+    profile_name = serializers.ReadOnlyField(source='owner.profile.display_name')
     featured_flag = serializers.ReadOnlyField()
     published_at = serializers.ReadOnlyField()
+    like_id = serializers.SerializerMethodField()
+    likes_count = serializers.ReadOnlyField()
+    comments_count = serializers.ReadOnlyField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
         return request.user == obj.owner
+
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, poem=obj
+            ).first()
+            return like.id if like else None
+        return None
+
 
     class Meta:
         model = Poem
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'profile_name', 'created_at', 'published_at',
-            'updated_at', 'title', 'content', 'category', 'published',
-            'featured_flag',
+            'updated_at', 'title', 'content', 'category', 'published', 'featured_flag',
+            'like_id', 'likes_count', 'comments_count',
         ]
--- poems/views.py
+++ poems/views.py
@@ -1,10 +1,11 @@
+
 from django.db.models import Count
 from rest_framework import generics, permissions, filters
 from django_filters.rest_framework import DjangoFilterBackend
 from django_filters import FilterSet, CharFilter
 from django_filters import rest_framework as drf_filters
+from drf_api.permissions import IsOwnerOrReadOnly
 from .models import Poem
-from rest_framework_api.permissions import IsOwnerOrReadOnly
 from .serializers import PoemSerializer
 
 
@@ -13,16 +14,46 @@ class PoemList(generics.ListCreateAPIView):
     List poems or create a poem if logged in.
     """
     serializer_class = PoemSerializer
-    queryset = Poem.objects.all().order_by('-created_at')
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset = Poem.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
+    filter_backends = [
+        filters.OrderingFilter,
+        filters.SearchFilter,
+        DjangoFilterBackend,
+    ]
+    filterset_fields = {
+        'owner__followed__owner__profile': ['exact'],
+        'likes__owner__profile': ['exact'],
+        'owner__profile': ['exact'],
+        'published_at': ['date__gte', 'date__lte'],
+        'category': ['exact'],
+        'likes_count': ['gte']
+    }
+    search_fields = (
+        'owner__profile_name',
+        'title',
+        'created_at'
+    )
+    ordering_fields = (
+        'likes_count',
+        'comments_count',
+        'likes__created_at',
+    )
 
     def perform_create(self, serializer):
-        serializer.save(owner=self.request.user)   
+        serializer.save(owner=self.request.user)          
 
 
 class PoemDetail(generics.RetrieveUpdateDestroyAPIView):
     """
     Retrieve a poem, or update or delete it by id if you own it.
     """
-    serializer_class = PoemSerializer
     permission_classes = [IsOwnerOrReadOnly]
-    queryset = Poem.objects.all().order_by('-created_at')
+    serializer_class = PoemSerializer
+    queryset = Poem.objects.annotate(
+        likes_count=Count('likes', distinct=True),
+        comments_count=Count('comment', distinct=True)
+    ).order_by('-created_at')
