e976ec6226891a0fd9e7f68ce365ce67635310c6
rkyzk <reikoyazaki@yahoo.com>
2023-07-27 12:54:01 +0000

    fix display time format for created_at & updated_at of comments

--- comments/serializers.py
+++ comments/serializers.py
@@ -16,6 +16,7 @@ class CommentSerializer(serializers.ModelSerializer):
     profile_name = serializers.ReadOnlyField(
                        source='owner.profile.display_name')
     created_at = serializers.SerializerMethodField()
+    updated_at = serializers.SerializerMethodField()
 
     def get_is_owner(self, obj):
         request = self.context['request']
@@ -24,14 +25,19 @@ class CommentSerializer(serializers.ModelSerializer):
     def get_created_at(self, obj):
         if obj.created_at.date() > datetime.now().date() - timedelta(days=7):
             return naturaltime(obj.created_at)
-        return obj.created_at
+        return obj.created_at.strftime("%d %b %Y")
+
+    def get_updated_at(self, obj):
+        if obj.updated_at.date() > datetime.now().date() - timedelta(days=7):
+            return naturaltime(obj.updated_at)
+        return obj.updated_at.strftime("%d %b %Y")
 
     class Meta:
         model = Comment
         fields = [
             'id', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'profile_name', 'poem', 'created_at',
-            'status', 'content'
+            'status', 'content', 'updated_at'
         ]
 
 
--- rest_framework_api/settings.py
+++ rest_framework_api/settings.py
@@ -66,7 +66,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 DEBUG = 'DEV' in os.environ
 
 ALLOWED_HOSTS = [
-    '8000-rkyzk-drfapi-wi7vo0777d0.ws-us101.gitpod.io',
+    '8000-rkyzk-drfapi-wi7vo0777d0.ws-us102.gitpod.io',
     os.environ.get('ALLOWED_HOST'),
     ]
 
